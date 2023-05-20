016e45e338c5186ec37eb775f9df8e15a2bb7168
GregHajduk <70976488+GregHajduk@users.noreply.github.com>
2023-05-20 22:17:09 +0100

    First commit

---
+++ src/main/java/com/FitFit/FitFit/FitFitApplication.java
@@ -0,0 +1,13 @@
+package com.FitFit.FitFit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitFitApplication.class, args);
+	}
+
+}
